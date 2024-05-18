73e6e8195847e847901d5d2ccf823b941a1b13e5
Sveltecult <sveltecult@proton.me>
2024-05-18 11:50:06 +0800

    fix divider height

--- src/lib/shadcn-ui/components/divider.ts
+++ src/lib/shadcn-ui/components/divider.ts
@@ -25,7 +25,8 @@ export default function (options: OptionsType = {}) {
 				backgroundImage: `url('data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%2220%22%20height%3D%2220%22%20viewBox%3D%220%200%2020%2020%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%0A%20%20%20%20%3Ccircle%20fill%3D%22none%22%20stroke%3D%22${encodeURIComponent(scheme.dark.border)}%22%20stroke-width%3D%222%22%20cx%3D%2210%22%20cy%3D%2210%22%20r%3D%227%22%20%2F%3E%0A%3C%2Fsvg%3E%0A')`
 			},
 			"[class*='uk-divider']": {
-				marginBottom: null
+				marginBottom: null,
+				height: '1px'
 			},
 			"* + [class*='uk-divider']": {
 				marginTop: null
