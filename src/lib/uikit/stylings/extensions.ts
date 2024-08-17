6d18d87506ef7aed8e2da34ae09a49db17d776d1
Sveltecult <sveltecult@proton.me>
2024-08-18 07:22:16 +0800

    add fake input

--- src/lib/uikit/stylings/extensions.ts
+++ src/lib/uikit/stylings/extensions.ts
@@ -86,6 +86,22 @@ export const rules: CSSRuleObject = {
 	},
 	'.uk-custom-select-search input': {
 		'@apply placeholder-muted-foreground w-full bg-transparent py-3 text-sm focus:outline-none': {}
+	},
+	'.uk-fake-input': {
+		'@apply border-input text-foreground flex h-9 w-full items-center rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm':
+			{}
+	},
+	'.uk-fake-input-focus, .uk-fake-input:focus': {
+		'@apply ring-ring outline-none ring-1': {}
+	},
+	'.uk-fake-input-focus.uk-form-danger, .uk-fake-input.uk-form-danger:focus': {
+		'@apply ring-destructive': {}
+	},
+	'.uk-fake-input:disabled': {
+		'@apply opacity-50': {}
+	},
+	'.uk-fake-input.uk-form-small': {
+		'@apply h-7 text-xs': {}
 	}
 };
 
