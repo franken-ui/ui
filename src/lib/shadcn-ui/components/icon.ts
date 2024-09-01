f00fe2acf171b60eaf35d72ad025d10b0f50166d
Sveltecult <sveltecult@proton.me>
2024-09-02 07:41:30 +0800

    fix icon and slidenav components

--- src/lib/shadcn-ui/components/icon.ts
+++ src/lib/shadcn-ui/components/icon.ts
@@ -1,7 +1,16 @@
 export default function () {
 	return {
 		'hook-icon': {
-			display: 'inline-flex'
+			margin: null,
+			border: null,
+			borderRadius: null,
+			overflow: null,
+			font: null,
+			textTransform: null,
+			padding: null,
+			backgroundColor: null,
+			display: 'inline-flex',
+			lineHeight: null
 		},
 		'hook-link': {
 			color: null
@@ -25,7 +34,7 @@ export default function () {
 			alignItems: null,
 			transition: null,
 			transitionProperty: null,
-			'@apply inline-flex h-9 w-9 items-center justify-center whitespace-nowrap rounded-md border border-input bg-background shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50':
+			'@apply inline-flex h-9 w-9 items-center justify-center rounded-md focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50':
 				{}
 		},
 		'hook-button-hover': {
@@ -35,7 +44,8 @@ export default function () {
 		},
 		'hook-button-active': {
 			backgroundColor: null,
-			color: null
+			color: null,
+			'@apply bg-accent text-accent-foreground': {}
 		},
 		'hook-misc': {
 			'.uk-icon-button-small': {
@@ -43,6 +53,9 @@ export default function () {
 			},
 			'.uk-icon-button-xsmall': {
 				'@apply h-7 w-7': {}
+			},
+			'.uk-icon-button-outline': {
+				'@apply border border-input shadow-sm': {}
 			}
 		}
 	};
--- src/lib/shadcn-ui/components/slidenav.ts
+++ src/lib/shadcn-ui/components/slidenav.ts
@@ -1,13 +1,25 @@
 export default function () {
 	return {
 		'hook-slidenav': {
-			color: null
+			padding: null,
+			color: null,
+			transition: null,
+			'@apply inline-flex h-9 w-9 items-center justify-center rounded-full border border-input shadow-sm bg-background focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring':
+				{}
 		},
 		'hook-hover': {
-			color: null
+			color: null,
+			'@apply bg-accent text-accent-foreground': {}
 		},
 		'hook-active': {
-			color: null
+			color: null,
+			'@apply bg-accent text-accent-foreground': {}
+		},
+		'hook-large': null,
+		'hook-misc': {
+			'.uk-slidenav svg': {
+				'@apply h-4 w-4': {}
+			}
 		}
 	};
 }
