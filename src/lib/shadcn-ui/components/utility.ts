a08430a57763e8c265d04ddf12fd24d787210468
Sveltecult <sveltecult@proton.me>
2024-09-02 11:39:48 +0800

    shadow adjustments

--- src/lib/shadcn-ui/components/utility.ts
+++ src/lib/shadcn-ui/components/utility.ts
@@ -13,6 +13,11 @@ export default function () {
 			color: null
 		},
 		'hook-misc': {
+			'.uk-panel-scrollable': {
+				height: null,
+				border: null,
+				'@apply h-40 border border-border': {}
+			},
 			'.uk-paragraph': {
 				'@apply leading-7 [&:not(:first-child)]:mt-6': {}
 			},
@@ -22,6 +27,39 @@ export default function () {
 			'.uk-codespan': {
 				'@apply relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold':
 					{}
+			},
+			'.uk-border-rounded': {
+				'@apply rounded-md': {}
+			},
+			'.uk-box-shadow-small': {
+				'@apply shadow-sm': {}
+			},
+			'.uk-box-shadow-medium': {
+				'@apply shadow-md': {}
+			},
+			'.uk-box-shadow': {
+				'@apply shadow': {}
+			},
+			'.uk-box-shadow-large': {
+				'@apply shadow-lg': {}
+			},
+			'.uk-box-shadow-xlarge': {
+				'@apply shadow-xl': {}
+			},
+			"[class*='uk-box-shadow-hover']": {
+				transition: null
+			},
+			'.uk-box-shadow-hover-small:hover': {
+				'@apply shadow-sm': {}
+			},
+			'.uk-box-shadow-hover-medium:hover': {
+				'@apply shadow-md': {}
+			},
+			'.uk-box-shadow-hover-large:hover': {
+				'@apply shadow-lg': {}
+			},
+			'.uk-box-shadow-hover-xlarge:hover': {
+				'@apply shadow-xl': {}
 			}
 		}
 	};
