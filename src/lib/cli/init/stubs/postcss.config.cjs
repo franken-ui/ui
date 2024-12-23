846c571e5297fb59a2d8ad51601cde0bc4d2be73
Sveltecult <sveltecult@proton.me>
2024-12-23 10:21:32 +0800

    remove postcss-sort-media-queries plugin on stub

--- src/lib/cli/init/stubs/postcss.config.cjs
+++ src/lib/cli/init/stubs/postcss.config.cjs
@@ -1,9 +1,6 @@
 module.exports = {
 	plugins: [
 		require('tailwindcss'),
-		require('franken-ui/postcss/sort-media-queries')({
-			sort: 'mobile-first'
-		}),
 		require('franken-ui/postcss/combine-duplicated-selectors')({
 			removeDuplicatedProperties: true
 		})
