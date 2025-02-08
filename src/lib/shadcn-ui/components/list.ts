1e9b223e008d01ccfe003b5d708fa1b7012c26a6
Sveltecult <sveltecult@proton.me>
2025-02-08 14:06:28 +0800

    fix list

--- src/lib/shadcn-ui/components/list.ts
+++ src/lib/shadcn-ui/components/list.ts
@@ -1,75 +1,56 @@
 export default {
-	'.uk-list': {
-		padding: '0',
-		listStyle: 'none'
-	},
 	'.uk-list > *': {
 		breakInside: 'avoid-column'
 	},
 	'.uk-list > * > :last-child': {
 		marginBottom: '0'
 	},
-	'.uk-list-disc > *, .uk-list-circle > *, .uk-list-square > *, .uk-list-decimal > *, .uk-list-hyphen > *':
-		{
-			'@apply ml-6': {}
-		},
-	'.uk-list-decimal': {
-		counterReset: 'decimal'
+	'.uk-list > :nth-child(n+2), .uk-list > * > ul': {
+		'@apply mt-2': {}
 	},
-	'.uk-list-decimal > *': {
-		counterIncrement: 'decimal'
+	'.uk-list-disc, .uk-list-circle, .uk-list-square, .uk-list-decimal, .uk-list-hyphen': {
+		paddingLeft: '32px'
 	},
-	'.uk-list-disc > ::before, .uk-list-circle > ::before, .uk-list-square > ::before, .uk-list-decimal > ::before, .uk-list-hyphen > ::before':
-		{
-			content: "''",
-			position: 'relative',
-			left: '-30px',
-			width: '30px',
-			height: '1.5em',
-			marginBottom: '-1.5em',
-			display: 'list-item',
-			listStylePosition: 'inside',
-			textAlign: 'right'
-		},
-	'.uk-list-disc > ::before': {
+	'.uk-list-disc': {
 		listStyleType: 'disc'
 	},
-	'.uk-list-circle > ::before': {
+	'.uk-list-circle': {
 		listStyleType: 'circle'
 	},
-	'.uk-list-square > ::before': {
+	'.uk-list-square': {
 		listStyleType: 'square'
 	},
-	'.uk-list-decimal > ::before': {
-		content: "counter(decimal, decimal) '\\200A.\\00A0'"
+	'.uk-list-decimal': {
+		listStyleType: 'decimal'
 	},
-	'.uk-list-hyphen > ::before': {
-		content: "'–\\00A0\\00A0'"
+	'.uk-list-hyphen': {
+		listStyleType: "'–  '"
 	},
-	'.uk-list-muted > ::before': {
+	'.uk-list-muted > ::marker': {
 		'@apply text-muted-foreground': {}
 	},
-	'.uk-list-primary > ::before': {
+	'.uk-list-primary > ::marker': {
 		'@apply text-primary': {}
 	},
-	'.uk-list-secondary > ::before': {
+	'.uk-list-secondary > ::marker': {
 		'@apply text-secondary-foreground': {}
 	},
 	'.uk-list-bullet > *': {
-		'@apply ml-6': {}
+		position: 'relative',
+		paddingLeft: '32px'
 	},
 	'.uk-list-bullet > ::before': {
-		content: "''",
-		display: 'list-item',
-		position: 'relative',
+		content: '""',
+		position: 'absolute',
+		top: '0',
+		left: '0',
+		width: '32px',
 		height: '1.5em',
-		marginBottom: '-1.5em',
 		backgroundImage: 'var(--uk-list-bullet-image)',
 		backgroundRepeat: 'no-repeat',
-		backgroundPosition: '50% 50%',
-		'@apply -left-6 w-6': {}
+		backgroundPosition: '50% 50%'
 	},
-	'.uk-list-divider > :nth-child(n + 2)': {
+	'.uk-list-divider > :nth-child(n+2)': {
 		'@apply mt-2 border-t border-border pt-2': {}
 	},
 	'.uk-list-striped > *': {
@@ -81,13 +62,7 @@ export default {
 	'.uk-list-striped > :nth-of-type(odd)': {
 		'@apply bg-muted/50': {}
 	},
-	'.uk-list-large.uk-list-striped > :nth-child(n + 2), .uk-list-collapse.uk-list-striped > :nth-child(n + 2)':
-		null,
-	'.uk-list-disc > li:not(:first-child), .uk-list-circle > li:not(:first-child), .uk-list-square > li:not(:first-child), .uk-list-decimal > li:not(:first-child), .uk-list-hyphen > li:not(:first-child)':
-		{
-			'@apply mt-2': {}
-		},
-	'.uk-list-bullet > li:not(:first-child)': {
-		'@apply mt-2': {}
+	'.uk-list-striped > :nth-child(n+2)': {
+		marginTop: '0'
 	}
 };
